5a2d303cfe7e13378f22216630c29057e7dbc2f0
havena <silveredenis@gmail.com>
2013-03-07 18:04:09 +0100

    ARP item

---
+++ ARP_ammobox/config.cpp
@@ -0,0 +1,583 @@
+class CfgPatches
+{
+	class ARP_ItemsCrate
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={"ARP_items"};
+	};
+};
+class cfgVehicles
+{
+	class ReammoBox;
+	class ARP_ItemsCrate:ReammoBox
+	{
+		scope=2;
+		accuracy=1000;
+		displayName="ARP All Items Crate";
+		model="\ca\weapons\AmmoBoxes\USBasicWeapons.p3d";
+		class TransportMagazines
+		{
+			class _xx_ARP_lance_grenade
+			{
+				Magazine="ARP_lance_grenade";
+				count=25;
+			};
+			class _xx_ARP_plantemedicinale
+			{
+				Magazine="ARP_plantemedicinale";
+				count=25;
+			};
+			class _xx_ARP_silencieux
+			{
+				Magazine="ARP_silencieux";
+				count=25;
+			};
+			class _xx_ARP_viseurPR
+			{
+				Magazine="ARP_viseurPR";
+				count=25;
+			};
+			class _xx_ARP_toleondulee
+			{
+				Magazine="ARP_toleondulee";
+				count=25;
+			};  
+			class _xx_ARP_batteries
+			{
+				Magazine="ARP_batteries";
+				count=25;
+			};
+            class _xx_ARP_ducttape
+			{
+				Magazine="ARP_ducttape";
+				count=25;
+			};
+            class _xx_ARP_steelpipe
+			{
+				Magazine="ARP_steelpipe";
+				count=25;
+			};
+            class _xx_ARP_wpnclnkit
+			{
+				Magazine="ARP_wpnclnkit";
+				count=25;
+			};
+            class _xx_ARP_m16_bolt
+			{
+				Magazine="ARP_m16_bolt";
+				count=25;
+			}; 
+            class _xx_ARP_m16_lower_receiver
+			{
+				Magazine="ARP_m16_lower_receiver";
+				count=25;
+			}; 
+			class _xx_ARP_AK_armature
+			{
+				Magazine="ARP_AK_armature";
+				count=25;
+			}; 
+			class _xx_ARP_AK_canon
+			{
+				Magazine="ARP_AK_canon";
+				count=25;
+			}; 
+			class _xx_ARP_AK_crosse
+			{
+				Magazine="ARP_AK_crosse";
+				count=25;
+			}; 
+			class _xx_ARP_Armature_2roues
+			{
+				Magazine="ARP_Armature_2roues";
+				count=25;
+			}; 
+			class _xx_ARP_Armature_4roues
+			{
+				Magazine="ARP_Armature_4roues";
+				count=25;
+			}; 
+			class _xx_ARP_batterie_vehicule
+			{
+				Magazine="ARP_batterie_vehicule";
+				count=25;
+			}; 
+			class _xx_ARP_caoutchouc
+			{
+				Magazine="ARP_caoutchouc";
+				count=25;
+			}; 
+			class _xx_ARP_chargeur_fusil_v
+			{
+				Magazine="ARP_chargeur_fusil_v";
+				count=25;
+			}; 
+			class _xx_ARP_chargeur_pistol_v
+			{
+				Magazine="ARP_chargeur_pistol_v";
+				count=25;
+			}; 
+			class _xx_ARP_digitale
+			{
+				Magazine="ARP_digitale";
+				count=25;
+			}; 
+			class _xx_ARP_douille_vide
+			{
+				Magazine="ARP_douille_vide";
+				count=25;
+			}; 
+			class _xx_ARP_exan
+			{
+				Magazine="ARP_exan";
+				count=25;
+			}; 
+			class _xx_ARP_fils_elec
+			{
+				Magazine="ARP_fils_elec";
+				count=25;
+			}; 
+			class _xx_ARP_GP_balles
+			{
+				Magazine="ARP_GP_balles";
+				count=25;
+			}; 
+			class _xx_ARP_GP_balles_sub
+			{
+				Magazine="ARP_GP_balles_sub";
+				count=25;
+			}; 
+			class _xx_ARP_GP_douille_metal
+			{
+				Magazine="ARP_GP_douille_metal";
+				count=25;
+			}; 
+			class _xx_ARP_GP_poudre
+			{
+				Magazine="ARP_GP_poudre";
+				count=25;
+			}; 
+			class _xx_ARP_graisse
+			{
+				Magazine="ARP_graisse";
+				count=25;
+			}; 
+			class _xx_ARP_M1014_armature
+			{
+				Magazine="ARP_M1014_armature";
+				count=25;
+			};	
+			class _xx_ARP_M1014_canon
+			{
+				Magazine="ARP_M1014_canon";
+				count=25;
+			};	
+			class _xx_ARP_M1014_crosse
+			{
+				Magazine="ARP_M1014_crosse";
+				count=25;
+			};	
+			class _xx_ARP_M16_armature
+			{
+				Magazine="ARP_M16_armature";
+				count=25;
+			};	
+			class _xx_ARP_M16_canon
+			{
+				Magazine="ARP_M16_canon";
+				count=25;
+			};	
+			class _xx_ARP_M4_armature
+			{
+				Magazine="ARP_M4_armature";
+				count=25;
+			};	
+			class _xx_ARP_M4_canon
+			{
+				Magazine="ARP_M4_canon";
+				count=25;
+			};	
+			class _xx_ARP_M4_crosse
+			{
+				Magazine="ARP_M4_crosse";
+				count=25;
+			};	
+			class _xx_ARP_pistolet_armature
+			{
+				Magazine="ARP_pistolet_armature";
+				count=25;
+			};	
+			class _xx_ARP_pistolet_canon
+			{
+				Magazine="ARP_pistolet_canon";
+				count=25;
+			};	
+			class _xx_ARP_piece_acier
+			{
+				Magazine="ARP_piece_acier";
+				count=25;
+			};	
+			class _xx_ARP_piece_metal
+			{
+				Magazine="ARP_piece_metal";
+				count=25;
+			};	
+			class _xx_ARP_plaque_acier
+			{
+				Magazine="ARP_plaque_acier";
+				count=25;
+			};	
+			class _xx_ARP_PP_balles
+			{
+				Magazine="ARP_PP_balles";
+				count=25;
+			};	
+			class _xx_ARP_PP_balles_sub
+			{
+				Magazine="ARP_PP_balles_sub";
+				count=25;
+			};	
+			class _xx_ARP_PP_douille_metal
+			{
+				Magazine="ARP_PP_douille_metal";
+				count=25;
+			};	
+			class _xx_ARP_PP_poudre
+			{
+				Magazine="ARP_PP_poudre";
+				count=25;
+			};	
+			class _xx_ARP_ressort
+			{
+				Magazine="ARP_ressort";
+				count=25;
+			};	
+			class _xx_ARP_Saiga_armature
+			{
+				Magazine="ARP_Saiga_armature";
+				count=25;
+			};	
+			class _xx_ARP_Saiga_canon
+			{
+				Magazine="ARP_Saiga_canon";
+				count=25;
+			};	
+			class _xx_ARP_Saiga_crosse
+			{
+				Magazine="ARP_Saiga_crosse";
+				count=25;
+			};	
+			class _xx_ARP_System_elec
+			{
+				Magazine="ARP_System_elec";
+				count=25;
+			};	
+			class _xx_ARP_tole_blindee
+			{
+				Magazine="ARP_tole_blindee";
+				count=25;
+			};	
+			class _xx_ARP_vis
+			{
+				Magazine="ARP_vis";
+				count=25;
+			};	
+			class _xx_ARP_tube_metal
+			{
+				Magazine="ARP_tube_metal";
+				count=25;
+			}; 
+			class _xx_ARP_PK_canon
+			{
+				Magazine="ARP_PK_canon";
+				count=25;
+			};
+			class _xx_ARP_PK_trepied
+			{
+				Magazine="ARP_PK_trepied";
+				count=25;
+			};
+			class _xx_ARP_PK_armature
+			{
+				Magazine="ARP_PK_armature";
+				count=25;
+			};
+			class _xx_ARP_PK_crosse
+			{
+				Magazine="ARP_PK_crosse";
+				count=25;
+			};
+			class _xx_ARP_M2_canon
+			{
+				Magazine="ARP_M2_canon";
+				count=25;
+			};
+			class _xx_ARP_M2_poignee
+			{
+				Magazine="ARP_M2_poignee";
+				count=25;
+			};
+			class _xx_ARP_M2_armature
+			{
+				Magazine="ARP_M2_armature";
+				count=25;
+			};
+			class _xx_ARP_M2
+			{
+				Magazine="ARP_M2";
+				count=25;
+			};
+			class _xx_ARP_socle_tournant
+			{
+				Magazine="ARP_socle_tournant";
+				count=25;
+			};
+			class _xx_ARP_pale_helico
+			{
+				Magazine="ARP_pale_helico";
+				count=25;
+			};
+			class _xx_ARP_canon_char
+			{
+				Magazine="ARP_canon_char";
+				count=25;
+			};
+			class _xx_ARP_siege
+			{
+				Magazine="ARP_siege";
+				count=25;
+			};
+			class _xx_ARP_chenille
+			{
+				Magazine="ARP_chenille";
+				count=25;
+			};
+			class _xx_ARP_traction
+			{
+				Magazine="ARP_traction";
+				count=25;
+			};
+			class _xx_ARP_derailleur
+			{
+				Magazine="ARP_derailleur";
+				count=25;
+			};
+			class _xx_ARP_plante_grasse
+			{
+				Magazine="ARP_plante_grasse";
+				count=25;
+			};
+			class _xx_ARP_salpetre
+			{
+				Magazine="ARP_salpetre";
+				count=25;
+			};
+			class _xx_ARP_cordage
+			{
+				Magazine="ARP_cordage";
+				count=25;
+			};
+			class _xx_ARP_phares
+			{
+				Magazine="ARP_phares";
+				count=25;
+			};
+			class _xx_ARP_craft_plastique
+			{
+				Magazine="ARP_craft_plastique";
+				count=25;
+			};
+			class _xx_ARP_mousse
+			{
+				Magazine="ARP_mousse";
+				count=25;
+			};
+			class _xx_ARP_filtre_cafe
+			{
+				Magazine="ARP_filtre_cafe";
+				count=25;
+			};
+			class _xx_ARP_plomb
+			{
+				Magazine="ARP_plomb";
+				count=25;
+			};
+			class _xx_ARP_chaine
+			{
+				Magazine="ARP_chaine";
+				count=25;
+			};
+			class _xx_ARP_plastique
+			{
+				Magazine="ARP_plastique";
+				count=25;
+			};
+			class _xx_ARP_mecanisme
+			{
+				Magazine="ARP_mecanisme";
+				count=25;
+			};
+			class _xx_ARP_tanboxer_dirty
+			{
+				Magazine="ARP_tanboxer_dirty";
+				count=25;
+			};
+			class _xx_ARP_bandana_black
+			{
+				Magazine="ARP_bandana_black";
+				count=25;
+			};
+			class _xx_ARP_disque_frein
+			{
+				Magazine="ARP_bandana_black";
+				count=25;
+			};
+			class _xx_ARP_acide
+			{
+				Magazine="ARP_bandana_black";
+				count=25;
+			};
+			class _xx_ARP_processeur
+			{
+				Magazine="ARP_processeur";
+				count=25;
+			};
+			class _xx_ARP_detcord
+			{
+				Magazine="ARP_detcord";
+				count=25;
+			};
+			class _xx_ARP_laser
+			{
+				Magazine="ARP_laser";
+				count=25;
+			};
+			class _xx_ARP_lentille
+			{
+				Magazine="ARP_lentille";
+				count=25;
+			};	
+			class _xx_ARP_cannabis
+			{
+				Magazine="ARP_cannabis";
+				count=25;
+			};
+			class _xx_ARP_culasse_rotative
+			{
+				Magazine="ARP_culasse_rotative";
+				count=25;
+			};
+			class _xx_ARP_gameboy
+			{
+				Magazine="ARP_gameboy";
+				count=25;
+			};
+		};
+		class TransportWeapons
+		{
+			class _xx_ARP_CLP
+			{
+				weapon="ARP_CLP";
+				count=25;
+			}; 
+			class _xx_ARP_kit_soudure
+			{
+				weapon="ARP_kit_soudure";
+				count=25;
+			};
+			class _xx_ARP_PK_montage
+			{
+				weapon="ARP_PK_montage";
+				count=25;
+			};	
+			 class _xx_ARP_playboy
+			{
+				weapon="ARP_playboy";
+				count=25;
+			};
+			class _xx_ARP_newspaper
+			{
+				weapon="ARP_newspaper";
+				count=25;
+			};
+			class _xx_ARP_picture_fam
+			{
+				weapon="ARP_picture_fam";
+				count=25;
+			};
+			class _xx_ARP_vodka
+			{
+				weapon="ARP_vodka";
+				count=25;
+			};
+			class _xx_ARP_suitcase_black
+			{
+				weapon="ARP_suitcase_black";
+				count=25;
+			};
+			class _xx_ARP_letter_norm
+			{
+				weapon="ARP_letter_norm";
+				count=25;
+			}; 
+			class _xx_ARP_dice
+			{
+				weapon="ARP_dice";
+				count=25;
+			};	
+			class _xx_ARP_leaflet_wanted
+			{
+				weapon="ARP_leaflet_wanted";
+				count=25;
+			}; 		
+			class _xx_ARP_cards
+			{
+				weapon="ARP_cards";
+				count=25;
+			};
+			class _xx_ARP_cigarettes
+			{
+				weapon="ARP_cigarettes";
+				count=25;
+			};
+			class _xx_ARP_ID
+			{
+				weapon="ARP_ID";
+				count=25;
+			};         
+			class _xx_ARP_picture_gf
+			{
+				weapon="ARP_picture_gf";
+				count=25;
+			};
+			class _xx_ARP_talky
+			{
+				weapon="ARP_talky";
+				count=25;
+			};
+			class _xx_ARP_papier
+			{
+				weapon="ARP_papier";
+				count=25;
+			};
+			class _xx_ARP_mouchoir
+			{
+				weapon="ARP_mouchoir";
+				count=25;
+			};
+			class _xx_ARP_figatellu	
+			{
+				weapon="ARP_figatellu";
+				count=25;
+			};
+			class _xx_ARP_joint
+			{
+				weapon="ARP_joint";
+				count=25;
+			};
+		}; 
+	};
+};
+
---
+++ ARP_items/config.cpp
@@ -0,0 +1,710 @@
+class CfgPatches {
+
+	class ARP_Items {
+		units[] = {};
+		weapons[] = {"ARP_CLP","ARP_kit_soudure","ARP_PK_montage",
+		"ARP_playboy","ARP_newspaper","ARP_picture_fam","ARP_vodka","ARP_suitcase_black","ARP_letter_norm",
+		"ARP_dice","ARP_leaflet_wanted","ARP_cards","ARP_cigarettes","ARP_ID"};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"CAWeapons"};
+	};
+};
+class CfgMagazines
+{
+class CA_Magazine;
+class ItemCoreDayZRP: CA_Magazine {
+
+	scope = 2;
+	count = 1;
+	ammo = "";
+	initSpeed = 0;
+	sound[] = {};
+  
+class Library
+  {
+   libTextDesc = "Objet DayZRP";
+   };
+};
+class ARP_lance_grenade: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Lance-grenade pour fusil";
+		picture="\arp_items\img\lance_grenade.paa";
+};
+class ARP_plantemedicinale: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Plante medicinale";
+		picture="\arp_items\img\plante_medicinale.paa";
+};	
+class ARP_silencieux: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Silencieux";
+		picture="\arp_items\img\Silencieux.paa";
+};
+class ARP_toleondulee: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Tole ondulee";
+		picture="\arp_items\img\toleondule.paa";
+};
+class ARP_viseurPR: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Viseur Point Rouge";
+		picture="\arp_items\img\viseur_PR.paa";
+};
+class ARP_batteries: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Piles";
+		picture="\arp_items\img\batteries.paa";
+		modelmagazine = "\ARP_items\3d\PILES.p3d";
+		model = "\ARP_items\3d\PILES.p3d";
+		modelspecial = "\ARP_items\3d\PILES.p3d";
+};
+class ARP_ducttape: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Ruban adhesif";
+		picture="\arp_items\img\ducttape.paa";
+};
+class ARP_exan: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Explosif";
+		picture="\arp_items\img\exan.paa";
+};
+class ARP_firestarter: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Pierre a feu";
+		picture="\arp_items\img\firestarter.paa";
+};
+class ARP_steelpipe: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Barre d'acier";
+		picture="\arp_items\img\steelpipe.paa";
+};
+class ARP_wpnclnkit: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Grand kit de montage";
+		picture="\arp_items\img\wpnclnkit.paa";
+};
+class ARP_m16_bolt: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Percuteur";
+		picture="\arp_items\img\m16_bolt.paa";
+};
+class ARP_Armature_2roues: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Cadre";
+		picture="\arp_items\img\armature_2_roues.paa";
+};
+class ARP_AK_armature: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Armature pour AK";
+		picture="\arp_items\img\AK_armature.paa";
+};
+class ARP_AK_canon: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Canon pour AK";
+		picture="\arp_items\img\AK_canon.paa";
+};
+class ARP_AK_crosse: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Crosse pour AK";
+		picture="\arp_items\img\AK_crosse.paa";
+};
+class ARP_Armature_4roues: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Chassis";
+		picture="\arp_items\img\armature_4_roues.paa";
+};
+class ARP_batterie_vehicule: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Batterie de voiture";
+		picture="\arp_items\img\batterie.paa";
+};
+class ARP_digitale: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Bouquet de digitale";
+		picture="\arp_items\img\bouquet_digitales.paa";
+};
+
+class ARP_douille_vide: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Douille de chasse vide";
+		picture="\arp_items\img\douille_vide.paa";
+};
+class ARP_fils_elec: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Fils electrique";
+		picture="\arp_items\img\fils_electriques.paa";
+};
+class ARP_GP_balles: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Grand pack de balles vides";
+		picture="\arp_items\img\grand_pack_balle.paa";
+};
+class ARP_GP_balles_sub: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Grand pack de balles subsoniques vides";
+		picture="\arp_items\img\grand_pack_balle_subs.paa";
+};
+class ARP_GP_douille_metal: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Grand pack de douilles metaliques vides";
+		picture="\arp_items\img\GP_douille_metallique.paa";
+};
+class ARP_GP_poudre: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Grand pack de poudre noire";
+		picture="\arp_items\img\GP_poudre_noire.paa";
+};
+class ARP_graisse: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Graisse";
+		picture="\arp_items\img\graisse.paa";
+};
+class ARP_M1014_armature: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Armature pour M1014";
+		picture="\arp_items\img\m1014_armature.paa";
+};
+class ARP_M1014_canon: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Canon pour M1014";
+		picture="\arp_items\img\m1014_canon.paa";
+};
+class ARP_M1014_crosse: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Crosse pour M1014";
+		picture="\arp_items\img\m1014_crosse.paa";
+};
+class ARP_M16_armature: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Armature pour M16";
+		picture="\arp_items\img\m16_armature.paa";
+};
+class ARP_M16_canon: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Canon pour M16";
+		picture="\arp_items\img\m16_canon.paa";
+};
+class ARP_m16_lower_receiver: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Crosse de M16";
+		picture="\arp_items\img\m16_lower_receiver.paa";
+};
+class ARP_M4_armature: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Armature pour M4";
+		picture="\arp_items\img\m4_armature.paa";
+};
+class ARP_M4_canon: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Canon pour M4";
+		picture="\arp_items\img\m4_canon.paa";
+};
+class ARP_M4_crosse: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Crosse pour M4";
+		picture="\arp_items\img\m4_crosse.paa";
+};
+class ARP_pistolet_armature: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Armature pour pistolet";
+		picture="\arp_items\img\m9_armature.paa";
+};
+class ARP_pistolet_canon: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Canon pour pistolet";
+		picture="\arp_items\img\m9_canon.paa";
+};
+class ARP_chargeur_fusil_v: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Chargeur pour fusil vide";
+		picture="\arp_items\img\Chargeur_fusil_v.paa";
+};
+class ARP_chargeur_pistol_v: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Chargeur pour pistolet vide";
+		picture="\arp_items\img\Chargeur_pistolet_v.paa";
+};
+class ARP_caoutchouc: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Morceaux de caoutchouc";
+		picture="\arp_items\img\caoutchouc.paa";
+};
+class ARP_piece_metal: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Pieces de metal";
+		picture="\arp_items\img\piece_metal.paa";
+};
+class ARP_piece_acier: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Pieces en acier";
+		picture="\arp_items\img\piece_acier.paa";
+};
+class ARP_plaque_acier: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Plaque en acier";
+		picture="\arp_items\img\plaque_acier.paa";
+};
+class ARP_PP_balles: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Petit pack de balles vides";
+		picture="\arp_items\img\petit_pack_balle.paa";
+};
+class ARP_PP_balles_sub: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Petit pack de balles subsoniques vides";
+		picture="\arp_items\img\petit_pack_balle_subs.paa";
+};
+class ARP_PP_douille_metal: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Petit pack de douilles métaliques vides";
+		picture="\arp_items\img\PP_douille-metal.paa";
+};
+class ARP_PP_poudre: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Petit pack de poudre noire";
+		picture="\arp_items\img\PP_poudre_noire.paa";
+};
+class ARP_ressort: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Ressort";
+		picture="\arp_items\img\ressort.paa";
+};
+class ARP_Saiga_armature: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Armature pour Saiga";
+		picture="\arp_items\img\Saiga_armature.paa";
+};
+class ARP_Saiga_canon: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Canon pour Saiga";
+		picture="\arp_items\img\Saiga_canon.paa";
+};
+class ARP_Saiga_crosse: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Crosse pour Saiga";
+		picture="\arp_items\img\Saiga_crosse.paa";
+};
+class ARP_System_elec: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Systeme electrique";
+		picture="\arp_items\img\systeme_elec.paa";
+};
+class ARP_tole_blindee: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Plaque blindee";
+		picture="\arp_items\img\tole_blinde.paa";
+};
+class ARP_vis: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Vis";
+		picture="\arp_items\img\vis.paa";
+};
+class ARP_tube_metal: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Tube en metal";
+		picture="\arp_items\img\Tube_metal.paa";
+};
+class ARP_PK_canon: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Canon pour PK";
+		picture="\arp_items\img\PK_canon.paa";
+};
+class ARP_PK_trepied: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Trepied pour PK";
+		picture="\arp_items\img\PK_trepied.paa";
+};
+class ARP_PK_armature: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Armature pour PK";
+		picture="\arp_items\img\PK_armature.paa";
+};
+class ARP_PK_crosse: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Crosse pour PK";
+		picture="\arp_items\img\PK_crosse.paa";
+};
+class ARP_M2: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="M2";
+		picture="\arp_items\img\M2.paa";
+};
+class ARP_M2_canon: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Canon pour M2";
+		picture="\arp_items\img\M2_canon.paa";
+};
+class ARP_M2_poignee: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Poignee pour M2";
+		picture="\arp_items\img\M2_poignee.paa";
+};
+class ARP_M2_armature: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Armature pour M2";
+		picture="\arp_items\img\M2_armature.paa";
+};
+class ARP_socle_tournant: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Socle tournant";
+		picture="\arp_items\img\socle_tournant.paa";
+};
+class ARP_pale_helico: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Pale d'helicoptere endommagee";
+		picture="\arp_items\img\pale_helico.paa";
+};
+class ARP_canon_char: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Morceaux de canon pour blindé";
+		picture="\arp_items\img\canon.paa";
+};
+class ARP_siege: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Siege de voiture";
+		picture="\arp_items\img\siege.paa";
+};
+class ARP_chenille: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Chenille pour blinde";
+		picture="\arp_items\img\chenille.paa";
+};
+class ARP_traction: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Systeme de traction";
+		picture="\arp_items\img\traction.paa";
+};
+class ARP_derailleur: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Derailleur pour velo";
+		picture="\arp_items\img\derailleur.paa";
+};
+class ARP_plante_grasse: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Plante grasse";
+		picture="\arp_items\img\plante_grasse.paa";
+};
+class ARP_salpetre: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Salpetre";
+		picture="\arp_items\img\salpetre.paa";
+};
+class ARP_cordage: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Cordage";
+		picture="\arp_items\img\cordage.paa";
+};
+class ARP_phares: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Phares";
+		picture="\arp_items\img\phares.paa";
+};
+class ARP_craft_plastique: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Plastique";
+		picture="\arp_items\img\craft_plastique.paa";
+};
+class ARP_mousse: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Lambeau de mousse";
+		picture="\arp_items\img\mousse.paa";
+};
+class ARP_filtre_cafe: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Filtre a cafe";
+		picture="\arp_items\img\filtre_cafe.paa";
+};
+class ARP_plomb: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Plomb";
+		picture="\arp_items\img\plomb.paa";
+};
+class ARP_chaine: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Chaine";
+		picture="\arp_items\img\chaine.paa";
+};
+class ARP_plastique: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Sac plastique";
+		picture="\arp_items\img\plastique.paa";
+};
+class ARP_mecanisme: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Mecanisme d'arme";
+		picture="\arp_items\img\mecanisme.paa";
+};
+class ARP_tanboxer_dirty: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Slip sale";
+		picture="\arp_items\img\tanboxer_dirty.paa";
+};
+class ARP_bandana_black: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Morceaux de tissu";
+		picture="\arp_items\img\bandana_black.paa";
+};
+class ARP_disque_frein: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Disque de frein";
+		picture="\arp_items\img\flywheel.paa";
+};
+class ARP_acide: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Acide";
+		picture="\arp_items\img\hydrochloric.paa";
+};
+class ARP_processeur: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Processeur";
+		picture="\arp_items\img\processeur.paa";
+};
+class ARP_detcord: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Cable electrique";
+		picture="\arp_items\img\detcord.paa";
+};
+class ARP_laser: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Pointeur laser";
+		picture="\arp_items\img\laser.paa";
+};
+class ARP_lentille: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Lentille";
+		picture="\arp_items\img\lentille.paa";
+};
+class ARP_cannabis: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Plante de cannabis";
+		picture="\arp_items\img\cannabis.paa";
+};
+class ARP_culasse_rotative: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Culasse rotative";
+		picture="\arp_items\img\Culasse_rotative.paa";
+};
+class ARP_gameboy: ItemCoreDayZRP
+{
+		scope=2;
+		displayName="Gameboy";
+		picture="\arp_items\img\gameboy.paa";
+};
+};
+class CfgWeapons
+{
+class Default;
+class ItemCore: Default {
+}; 
+class ARP_CLP: ItemCore
+{
+		scope=2;
+		displayName="Colle";
+		picture="\arp_items\img\CLP.paa";
+};
+class ARP_kit_soudure: ItemCore
+{
+		scope=2;
+		displayName="Kit de soudure";
+		picture="\arp_items\img\kit_soudure.paa";
+};
+class ARP_PK_montage: ItemCore
+{
+		scope=2;
+		displayName="Petit kit de montage";
+		picture="\arp_items\img\Petit_kit_MA.paa";
+};
+class ARP_playboy: ItemCore
+{
+		scope=2;
+		displayName="Magazine Playboy";
+		picture="\arp_items\img\playboy.paa";
+};
+class ARP_newspaper: ItemCore
+{
+		scope=2;
+		displayName="Journal";
+		picture="\arp_items\img\newspaper.paa";
+};
+class ARP_picture_fam: ItemCore
+{
+		scope=2;
+		displayName="Photo de famille";
+		picture="\arp_items\img\picture_fam.paa";
+};
+class ARP_vodka: ItemCore
+{
+		scope=2;
+		displayName="Vodka";
+		picture="\arp_items\img\vodka.paa";
+};
+class ARP_suitcase_black: ItemCore
+{
+		scope=2;
+		displayName="Malette noire";
+		picture="\arp_items\img\suitcase_black.paa";
+};
+class ARP_letter_norm: ItemCore
+{
+		scope=2;
+		displayName="Lettre";
+		picture="\arp_items\img\letter_norm.paa";
+};
+class ARP_dice: ItemCore
+{
+		scope=2;
+		displayName="Des";
+		picture="\arp_items\img\dice.paa";
+};
+class ARP_leaflet_wanted: ItemCore
+{
+		scope=2;
+		displayName="Avis de recherche";
+		picture="\arp_items\img\leaflet_wanted.paa";
+};
+class ARP_cards: ItemCore
+{
+		scope=2;
+		displayName="Carte a jouer";
+		picture="\arp_items\img\cards.paa";
+};
+class ARP_cigarettes: ItemCore
+{
+		scope=2;
+		displayName="Cigarettes";
+		picture="\arp_items\img\cigarettes.paa";
+};
+class ARP_ID: ItemCore
+{
+		scope=2;
+		displayName="Carte d'identite";
+		picture="\arp_items\img\ID_card.paa";
+};
+class ARP_picture_gf: ItemCore
+{
+		scope=2;
+		displayName="Photo de copine";
+		picture="\arp_items\img\picture_gf.paa";
+};
+class ARP_talky: ItemCore
+{
+		scope=2;
+		displayName="Talky-walky defecteux";
+		picture="\arp_items\img\2wayradio.paa";
+};
+class ARP_papier: ItemCore
+{
+		scope=2;
+		displayName="Papier toilette";
+		picture="\arp_items\img\papier.paa";
+};
+class ARP_mouchoir: ItemCore
+{
+		scope=2;
+		displayName="Mouchoir usager";
+		picture="\arp_items\img\mouchoir.paa";
+};
+class ARP_figatellu: ItemCore
+{
+		scope=2;
+		displayName="Figatellu";
+		picture="\arp_items\img\figatellu.paa";
+};
+class ARP_joint: ItemCore
+{
+		scope=2;
+		displayName="Joint";
+		picture="\arp_items\img\joint.paa";
+};
+};

8c6787ddf71c3e5ed81b8e72bb387f31dd6bda67
Raztoux <tom_belier92i@hotmail.com>
2013-03-07 16:59:35 +0100

    Ajout item "ARP Special DayZRP"
    - Ajout d'items "ARP Special DayZRP"
    
    ARP_fils_elec
    ARP_plastique
    ARP_newspaper
    ARP_mouchoir
    ARP_papier
    ARP_picture_fam
    ARP_vodka
    ARP_letter_norm
    ARP_dice
    ARP_leaflet_wanted
    ARP_cigarettes
    ARP_ID
    ARP_tanboxer_dirty
    ARP_batteries
    ARP_filtre_cafe
    ARP_figatellu
    ARP_gameboy
    ARP_chaine
    ARP_playboy

--- dayz_code/cfgLoot.hpp
+++ dayz_code/cfgLoot.hpp
@@ -3,12 +3,22 @@ class CfgLoot {
 		{
 			"TrashTinCan",
 			"TrashJackDaniels",
-			"ItemSodaEmpty"
+			"ItemSodaEmpty",
+			"ARP_fils_elec",
+			"ARP_newspaper",
+			"ARP_plastique",
+			"ARP_mouchoir",
+			"ARP_papier"
 		},
 		{
-			0.05,	//	"TrashTinCan",
-			0.01,	//	"TrashJackDaniels",
-			0.05	//	"ItemSodaEmpty"
+			0.05,	// "TrashTinCan",
+			0.01,	// "TrashJackDaniels",
+			0.05,	// "ItemSodaEmpty",
+			0.02,	// "ARP_fils_elec",
+			0.03,	// "ARP_newspaper",
+			0.02,	// "ARP_plastique",
+			0.02,	// "ARP_mouchoir",
+			0.03	// "ARP_papier"
 		}
 	};
 	civilian[] = {
@@ -23,15 +33,14 @@ class CfgLoot {
 			"ItemPainkiller"
 		},
 		{
-			0.06,	//	"TrashTinCan",
-			0.01,	//	"TrashJackDaniels",
-			0.06,	//	"ItemSodaEmpty",
-			0.01,	//	"8Rnd_9x18_Makarov",
-			0.01,	//	"Single_W1866_Slug",
-			0.01,	//	"Single_Rnd_Pellet",
-			0.01,	//	"ItemBandage",
-			0.01	//	"ItemPainkiller"
-		}
+			0.06,	// "TrashTinCan",
+			0.01,	// "TrashJackDaniels",
+			0.06,	// "ItemSodaEmpty",
+			0.01,	// "8Rnd_9x18_Makarov",
+			0.01,	// "Single_W1866_Slug",
+			0.01,	// "Single_Rnd_Pellet",
+			0.01,	// "ItemBandage",
+			0.01	// "ItemPainkiller"
 	};
 	food[] = {
 		{
@@ -44,13 +53,13 @@ class CfgLoot {
 			"ItemBandage"
 		},
 		{
-			0.06,	//	"TrashTinCan",
-			0.05,	//	"ItemSodaEmpty",
-			0.01,	//	"FoodCanBakedBeans",
-			0.01,	//	"FoodCanSardines",
-			0.01,	//	"FoodCanFrankBeans",
-			0.02,	//	"FoodCanDogFood",
-			0.01	//	"ItemBandage"
+			0.06,	// "TrashTinCan",
+			0.05,	// "ItemSodaEmpty",
+			0.01,	// "FoodCanBakedBeans",
+			0.01,	// "FoodCanSardines",
+			0.01,	// "FoodCanFrankBeans",
+			0.02,	// "FoodCanDogFood",
+			0.01	// "ItemBandage"
 		}
 	};
 	drink[] = {
@@ -82,27 +91,55 @@ class CfgLoot {
 			"HandChemGreen",
 			"HandChemBlue",
 			"HandChemRed",
-			"ItemHeatPack"
+			"ItemHeatPack",
+			"ARP_picture_fam",
+			"ARP_vodka",
+			"ARP_playboy",
+			"ARP_letter_norm",
+			"ARP_dice",
+			"ARP_leaflet_wanted",
+			"ARP_cigarettes",
+			"ARP_ID",
+			"ARP_tanboxer_dirty",
+			"ARP_batteries",
+			"ARP_filtre_cafe",
+			"ARP_figatellu",
+			"ARP_gameboy",
+			"ARP_chaine"
 		},
 		{
-			0.16,	//	"TrashTinCan",
-			0.16,	//	"ItemSodaEmpty",
-			0.01,	//	"TrashJackDaniels",
-			0.02,	//	"ItemWaterbottleUnfilled",
-			0.01,	//	"ItemBandage",
-			0.01,	//	"mosin_bullet",
-			0.01,	//	"8Rnd_TT",
-			0.01,	//	"Single_W1866_Slug",
-			0.01,	//	"Single_Rnd_Pellet",
-			0.01,	//	"8Rnd_9x18_Makarov",
-			0.01,	//	"Single_W1866_Slug",
-			0.01,	//	"BoltSteel",
-			0.01,	//	"HandRoadFlare",
-			0.01,	//	"ItemPainkiller",
-			0.01,	//	"HandChemGreen",
-			0.01,	//	"HandChemBlue",
-			0.01,	//	"HandChemRed",
-			0.01	//	"ItemHeatPack"			
+			0.16,	// "TrashTinCan",
+			0.16,	// "ItemSodaEmpty",
+			0.01,	// "TrashJackDaniels",
+			0.02,	// "ItemWaterbottleUnfilled",
+			0.01,	// "ItemBandage",
+			0.01,	// "mosin_bullet",
+			0.01,	// "8Rnd_TT",
+			0.01,	// "Single_W1866_Slug",
+			0.01,	// "Single_Rnd_Pellet",
+			0.01,	// "8Rnd_9x18_Makarov",
+			0.01,	// "Single_W1866_Slug",
+			0.01,	// "BoltSteel",
+			0.01,	// "HandRoadFlare",
+			0.01,	// "ItemPainkiller",
+			0.01,	// "HandChemGreen",
+			0.01,	// "HandChemBlue",
+			0.01,	// "HandChemRed",
+			0.01,	// "ItemHeatPack",
+			0.01,	// "ARP_picture_fam",
+			0.01,	// "ARP_vodka",
+			0.01,	// "ARP_playboy",
+			0.01,	// "ARP_letter_norm",
+			0.01,	// "ARP_dice",
+			0.01,	// "ARP_leaflet_wanted",
+			0.01,	// "ARP_cigarettes",
+			0.01,	// "ARP_ID",
+			0.02,	// "ARP_tanboxer_dirty",
+			0.02,	// "ARP_batteries",
+			0.01,	//  "ARP_filtre_cafe",
+			0.01,	// "ARP_figatellu",
+			0.02,	// "ARP_gameboy",
+			0.02	// "ARP_chaine"          
 		}
 	};
 	medical[] = {
@@ -111,14 +148,14 @@ class CfgLoot {
 			"ItemPainkiller",
 			"ItemMorphine",
 			"ItemEpinephrine",
-			"ItemHeatPack"
+			"ItemHeatPack" 
 		},
 		{
-			0.01,	//	"ItemBandage",
-			0.01,	//	"ItemPainkiller",
-			0.01,	//	"ItemMorphine",
-			0.01,	//	"ItemEpinephrine",
-			0.01	//	"ItemHeatPack"
+			0.01,	// "ItemBandage",
+			0.01,	// "ItemPainkiller",
+			0.01,	// "ItemMorphine",
+			0.01,	// "ItemEpinephrine",
+			0.01	// "ItemHeatPack"
 		}
 	};
 	hospital[] = {
@@ -130,11 +167,11 @@ class CfgLoot {
 			"ItemAntibiotic"
 		},
 		{
-			0.01,	//	"ItemBandage",
-			0.01,	//	"ItemPainkiller",
-			0.01,	//	"ItemMorphine",
-			0.01,	//	"ItemEpinephrine",
-			0.01	//	"ItemAntibiotic"
+			0.01,	// "ItemBandage",
+			0.01,	// "ItemPainkiller",
+			0.01,	// "ItemMorphine",
+			0.01,	// "ItemEpinephrine",
+			0.01	// "ItemAntibiotic"
 		}
 	};
 	military[] = {
@@ -158,39 +195,37 @@ class CfgLoot {
 			"ice_60Rnd_556x45_Stanag_tape"
 		},
 		{
-			0.17,	//	"TrashTinCan",
-			0.17,	//	"ItemSodaEmpty",
-			0.01,	//	"ItemBandage",
-			0.01,	//	"ItemPainkiller",
-			0.01,	//	"ItemMorphine",
-			0.01,	//	"SmokeShell",
-			0.01,	//	"SmokeShellRed",
-			0.01,	//	"SmokeShellGreen",
-			0.01,	//	"HandChemGreen",
-			0.01,	//	"HandChemBlue",
-			0.01,	//	"HandChemRed",
-			0.01,	//	"ItemHeatPack"
-			0.01,	//	"8Rnd_9x18_Makarov",
-			0.01,	//	"Single_W1866_Slug",
-			0.01,	//	"8Rnd_TT",
-			0.01,	//	"mosin_bullet",
-			0.01,	//	"15Rnd_556x45_Stanag_tape"
-
+			0.17,	// "TrashTinCan",
+			0.17,	// "ItemSodaEmpty",
+			0.01,	// "ItemBandage",
+			0.01,	// "ItemPainkiller",
+			0.01,	// "ItemMorphine",
+			0.01,	// "SmokeShell",
+			0.01,	// "SmokeShellRed",
+			0.01,	// "SmokeShellGreen",
+			0.01,	// "HandChemGreen",
+			0.01,	// "HandChemBlue",
+			0.01,	// "HandChemRed",
+			0.01,	// "ItemHeatPack"
+			0.01,	// "8Rnd_9x18_Makarov",
+			0.01,	// "Single_W1866_Slug",
+			0.01,	// "8Rnd_TT",
+			0.01,	// "mosin_bullet",
+			0.01	// "15Rnd_556x45_Stanag_tape"
 		};
 	};
 	policeman[] = {
 		{
 			"ItemBandage",
-
 			"8Rnd_TT",
 			"Single_W1866_Slug",
 			"HandRoadFlare"
 		},
 		{
-			0.01,	//	"ItemBandage",
-			0.01,	//	"8Rnd_TT",
-			0.01,	//	"Single_W1866_Slug",
-			0.01	//	"HandRoadFlare"
+			0.01,	// "ItemBandage",
+			0.01,	// "8Rnd_TT",
+			0.01,	// "Single_W1866_Slug",
+			0.01	// "HandRoadFlare"
 		}
 	};
 	hunter[] = {
@@ -201,10 +236,10 @@ class CfgLoot {
 			"ItemHeatPack"
 		},
 		{
-			0.01,	//	"ItemBandage",
-			0.01,	//	"ItemWaterbottleUnfilled",
-			0.01,	//	"BoltSteel",
-			0.01	//	"ItemHeatPack"
+			0.01,	// "ItemBandage",
+			0.01,	// "ItemWaterbottleUnfilled",
+			0.01,	// "BoltSteel",
+			0.01	// "ItemHeatPack"
 		}
 	};
 };
